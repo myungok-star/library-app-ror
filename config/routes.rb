68266374ce14eeeda2dbec183663a7d89e408fc5
myungok-star <myungok.star@gmail.com>
2016-12-20 21:00:44 -0800

    POST /users creates a new user in the database

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'users#index'
   get '/users', to: 'users#index'
   get '/users/new', to: 'users#new', as: 'new_user'
+  post '/users', to: 'users#create'
 end
